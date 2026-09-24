fe2337eb773323e01c7316bd9bf9fe31f23ce687
Jonathan Morales <jonathansme01@gmail.com>
2026-09-24 11:54:41 -0600

    feat: define weather search foundations

---
+++ src/features/weather/constants/countries.ts
@@ -0,0 +1,8 @@
+export const COUNTRIES = [
+  { code: "MX", name: "México" },
+  { code: "US", name: "Estados Unidos" },
+  { code: "AR", name: "Argentina" },
+  { code: "CO", name: "Colombia" },
+  { code: "ES", name: "España" },
+  { code: "PE", name: "Perú" },
+] as const;
