6b03fb40aca8a7c9725d3b87a73ba8c7e3e386d8
Jonathan Morales <jonathansme01@gmail.com>
2026-09-25 14:21:41 -0600

    feat: integrate weather server state

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,13 @@ import { createRoot } from "react-dom/client";
 import "@fontsource-variable/raleway";
 import "./index.css";
 
+import AppProviders from "./app/providers";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <AppProviders>
+      <App />
+    </AppProviders>
   </StrictMode>,
 );
